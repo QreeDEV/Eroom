8a2d96abf2834917ddc87994612d78809b67a19b
Qree_Dev <102918578+QreeDEV@users.noreply.github.com>
2023-02-17 21:08:02 +0900

    Update list.js

--- list.js
+++ list.js
@@ -5,8 +5,8 @@ let allMusic = [
         audio : "e-1"
     },
     {   
-        name : "본문 1-2"
-        artist : "...."
+        name : "본문 1-2",
+        artist : "....",
         audio : "e-2"
     },
 ]
